fbd50835b251d09403e0228830dbf0291259bc7f
Raggdoll <Cozartkevin@gmail.com>
2015-11-30 20:38:38 -0800

    fuck

--- js/play.js
+++ js/play.js
@@ -1,9 +1,10 @@
 var playState = {
 create: function(){
 	this.totalScore = 0;
-	this.CurrentScore = this.game.add.text(this.game.camera.width/2, 575, "Score: " + this.totalScore );
-	this.CurrentScore.anchor.setTo(0.5,0.5);
+	this.CurrentScore = game.add.text(this.game.camera.width/2, 575, "Score: " + this.totalScore );
+		this.CurrentScore.anchor.setTo(0.5,0.5);
 this.CurrentScore.fixedToCamera = true;
+
 	 bullets = game.add.group();
     bullets.enableBody = true;
     bullets.physicsBodyType = Phaser.Physics.ARCADE;
@@ -30,24 +31,55 @@ this.CurrentScore.fixedToCamera = true;
 	this.player.animations.add('player_jump_left', [54],10,true);
 
 	
-	//enemy code	
-//	this.enemy = game.add.group();
-//	this.enemy.setAll('checkWorldBoudns', true);
-//	this.enemy.setAll('outOfBoundsKill', true);
-//	this.enemy.createMultiple(15, 'enemy');
-	this.enemy = game.add.sprite((this.player.x + 475), 400, 'enemy');
+this.enemy = game.add.sprite((this.player.x + 475), 450, 'enemy');
+	this.enemyPool = this.add.group();
+    this.enemyPool.enableBody = true;
+    this.enemyPool.physicsBodyType = Phaser.Physics.ARCADE;
+    this.enemyPool.createMultiple(50, 'enemy');
+    this.enemyPool.setAll('anchor.x', 0.5);
+    this.enemyPool.setAll('anchor.y', 0.5);
+    this.enemyPool.setAll('outOfBoundsKill', true);
+    this.enemyPool.setAll('checkWorldBounds', true);
+
+function increaseScore(totalScore,CurrentScore){
+	if(this.enemyFacing == 'right'){
+		this.enemy.animations.play('die_right',15, false, true);
+		
+	} else{
+		this.enemy.animations.play('die_left',15, false, true);
+		
+	}
+game.time.events.remove(moveEnemy);
+	updateScore();
+	generateEnemy();
+
+	
+}	
+
+    // Set the animation for each sprite
+    this.enemyPool.forEach(function (enemy) {
+
+	enemy.animations.add('walk_right', [0,1,2,3], 3, true);
+	enemy.animations.add('walk_left', [6,7,8,9], 3, true);
+	enemy.animations.add('die_right', [12,13,14,15,16,17], 15, false);
+	enemy.animations.add('die_left', [18,19,20,21,22,23], 15, false);
+	enemy.body.velocity.x = -25;
+		});
+	this.nextEnemyAt = 0;
+    this.enemyDelay = 2500;
+	this.physics.arcade.overlap(bullets,this.enemyPool, increaseScore, null, this);
 	this.enemy.animations.add('walk_right', [0,1,2,3], 3, true);
-	this.enemy.animations.add('walk_left', [7,8,9,10], 3, true);
-	this.enemy.animations.add('die_right', [12,13,14,15,16,17], 3, false);
-	this.enemy.animations.add('die_left', [18,19,20,21,22,23], 3, false);
-	this.enemyFacing = 'left';
+	this.enemy.animations.add('walk_left', [6,7,8,9], 3, true);
+	this.enemy.animations.add('die_right', [12,13,14,15,16,17], 15, false);
+	this.enemy.animations.add('die_left', [18,19,20,21,22,23], 15, false);
+this.enemyFacing = 'left';
 	game.physics.enable(this.enemy, Phaser.Physics.ARCADE);
 	this.enemy.anchor.setTo(0.5,0.5);
-	this.enemy.rangeLeft = 15;
-	this.enemy.rangeRight = 15;
-		
-	
-	
+	this.enemy.range = 5;
+	isFirstRun = true;
+
+
+
 	this.directionFacing = 'right';
 	game.physics.arcade.gravity.y = 250;
 	game.physics.enable(this.player, Phaser.Physics.ARCADE);
@@ -57,94 +89,106 @@ this.CurrentScore.fixedToCamera = true;
 	//this.player.body.bounce.y = 0.8;
 	//this.player.body.allowGravity = true;
 	 // game.camera.follow(backGround);
-	this.win = game.add.sprite(500, 256, 'win');
+	//this.win = game.add.sprite(500, 256, 'win');
 	//game.physics.enable(this.win, Phaser.Physics.ARCADE);
 	
 	//this.player.boundsPadding = -10;
 },
 update: function(){
+	
+	
+	game.physics.arcade.collide(this.player, layer);
+	game.physics.arcade.collide(this.enemyPool, layer);
 	game.physics.arcade.collide(this.enemy, layer);
+	
+	
+	if (this.nextEnemyAt < this.time.now && this.enemyPool.countDead() > 0) {
+      this.nextEnemyAt = this.time.now + this.enemyDelay;
+      var enemy = this.enemyPool.getFirstExists(false);
+      // spawn at a random location top of the screen
+      enemy.reset(this.rnd.integerInRange(scrollPos + 300, scrollPos + 600), 400);
+      // also randomize the speed
+	}
+   if(this.enemyFacing == 'right'){
+		enemy.play('die_right');
+		enemy.body.velocity.x = 25;
+		} else {
+			enemy.play('die_left');
+			enemy.body.velocity.x = -25;
+		 
+    }
 	
 	function generateEnemy(){
-			this.enemy.reset(scrollPos + 400, 400);
 		
-//		this.enemy = game.add.sprite(scrollPos + 500, 400, 'enemy');
-//	this.enemy.animations.add('walk_right', [0,1,2,3], 3, true);
-//	this.enemy.animations.add('walk_left', [7,8,9,10], 3, true);
-//	this.enemy.animations.add('die_right', [12,13,14,15,16,17], 3, false);
-//	this.enemy.animations.add('die_left', [18,19,20,21,22,23], 3, false);
-//	this.enemyFacing = 'left';
-//	game.physics.enable(this.enemy, Phaser.Physics.ARCADE);
+	this.enemy = game.add.sprite((scrollPos + 475), 400, 'enemy');
+//this.enemy.animations.add('walk_right', [0,1,2,3], 3, true);
+//this.enemy.animations.add('walk_left', [7,8,9,10], 3, true);
+//this.enemy.animations.add('die_right', [12,13,14,15,16,17], 3, false);
+//this.enemy.animations.add('die_left', [18,19,20,21,22,23], 3, false);
+	//this.enemyFacing = 'left';
+	game.physics.enable(this.enemy, Phaser.Physics.ARCADE);
 	
-//	this.enemy.anchor.setTo(0.5,0.5);
-//	this.enemy.rangeLeft = 15;
-//	this.enemy.rangeRight = 15;
-//	game.physics.arcade.collide(this.enemy, layer);
-//	this.enemy.revive();
+	//this.enemy.anchor.setTo(0.5,0.5);
+	//this.enemy.rangeLeft = 15;
+	//this.enemy.rangeRight = 15;
+	game.physics.arcade.collide(this.enemy, layer);
+//this.enemy.revive();
 		}
 		
-	if(this.totalScore == 300){
+	if(playState.totalScore >= 300){
 		this.Win;
 	}
-	game.physics.arcade.collide(this.player, layer);
-	game.physics.arcade.collide(this.enemy, layer);
+
 	
-	
-		if(this.enemyFacing == 'right'){
-			this.enemy.animations.play('walk_right');
-			this.enemy.body.velocity.x = 25;
-		} else {
-			this.enemy.animations.play('walk_left');
-			this.enemy.body.velocity.x = -25;
-		} 
+	  //  
+		//} 
 		
 		
 		
  function moveEnemy(){
-	baddieMover = game.rnd.integerInRange(1, 3);
+	baddieMover = game.rnd.integerInRange(1, 2);
 	 //code stolen then modified from help request on html5gamedevs. http://www.html5gamedevs.com/topic/7706-please-help-with-enemy-movement-using-a-timer/
-	  if (this.enemy.x < this.enemy.rangeLeft && this.baddieMover == 2) {
-          this.baddieMover = 1; // enemy is too far left, so move it right
+	  if (this.enemy.x  > this.enemy.startX - this.enemy.range && baddieMover == 2) {
+          baddieMover = 1; // enemy is too far left, so move it right
         }
-        else if (this.enemy.x > this.enemy.rangeRight && this.BaddieMover == 1) {
-          this.baddieMover = 2; // enemy is too far right, so move it left
+        else if (this.enemy.x  < this.enemy.startX + this.enemy.range && baddieMover == 1) {
+          baddieMover = 2; // enemy is too far right, so move it left
         }
-
         if (this.baddieMover == 1) {
 		this.enemy.body.velocity.x = 25;
-		this.enemy.animations.play('walk_right');
+		this.enemy.animations.play('walk_right', 3, true, false);
 	}
 	else if (this.baddieMover == 2) {
 		this.enemy.body.velocity.x = -25;
-		this.enemy.animations.play('walk_left');
+		this.enemy.animations.play('walk_left', 3, true, false);
 	}
 	else {
-		this.enemy.body.velocity.x = 0;
-		this.enemy.animations.stop();
+		//this.enemy.body.velocity.x = 0;
+		//this.enemy.animations.stop();
 	}
  }
-game.time.events.loop(Phaser.Timer.SECOND, moveEnemy, this); 
+game.time.events.loop(Phaser.Timer.SECOND *3,moveEnemy, this); 
 	
 	scrollPos = scrollPos + 0.25;
-	game.physics.arcade.overlap(bullets, this.enemy, increaseScore, null, this);
+	//game.physics.arcade.overlap(bullets, this.enemy, increaseScore, null,this);
 
-	
-function increaseScore(){
-	if(this.enemyFacing = 'right'){
-		this.enemy.animations.play('die_right');
-		this.enemy.kill();
-	} else{
-		this.enemy.animations.play('die_left');
-		this.enemy.kill();
+	if(isFirstRun == true){
+		this.enemy.animations.play('walk_left',3,true,false);
+		enemy.body.velocity.x = -25
+		isFirstRun = false;
 	}
-	this.totalScore = this.totalScore + 100;
 	
-	generateEnemy();
-
 	
-}	
+	
 
-	 game.physics.arcade.overlap(this.player, this.win, this.Win, null, this);
+
+function updateScore(totalScore, CurrentScore){
+	playState.totalScore = playState.totalScore + 100;
+	playState.CurrentScore= ("Score: " + playState.totalScore );
+
+}
+
+	 //game.physics.arcade.overlap(this.player, this.win, this.Win, null, this);
 	backGround.position.x = (scrollPos);
 	game.camera.x = scrollPos;
 
@@ -157,7 +201,7 @@ function increaseScore(){
 	Theta /= (Math.PI/4);
 	Theta = Math.round(Theta);
 	Theta *= Math.PI/4;
-	console.log(Theta);
+	//console.log(Theta);
 	
   switch(Theta){
 	  case 0:
